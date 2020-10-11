64175000c8f4f9365e2a43542d3883309c99cc0f
YorkLi <kmgs4524@gmail.com>
2020-10-11 21:54:16 +0800

    修正 bug + 重構 code
    1. 修正從目錄點擊播放時，若有正在播放的媒體會有聲音中斷的現象
    2. 修正播放完最後一首時會閃退
    3. 重構 MainActivity, PlayService, SongAdapter

--- app/src/main/java/com/york/android/musicsession/view/exoplayer/SongAdapter.kt
+++ app/src/main/java/com/york/android/musicsession/view/exoplayer/SongAdapter.kt
@@ -15,10 +15,6 @@ import com.york.android.musicsession.view.MainActivity
  * Created by York on 2018/3/20.
  */
 class SongAdapter(val songs: List<Song>, val context: Context): RecyclerView.Adapter<SongAdapter.SongItemHolder>() {
-    init {
-        setService(songs)
-        Log.d("onBindViewHolder", "init: setService")
-    }
 
     override fun onBindViewHolder(holder: SongItemHolder?, position: Int) {
         holder?.bind(songs[position])
@@ -26,27 +22,17 @@ class SongAdapter(val songs: List<Song>, val context: Context): RecyclerView.Ada
             Log.d("bind", "position: ${position} song path: ${songs[position].filePath}")
 //            (context as MainActivity).verifyStoragePermission()
             (context as MainActivity).setPlaylist(songs, position)
-            context.playMedia(position)
             context.showBottomPlayerControl()
-//            context.onDisplaySong()
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): SongItemHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.songitem_recyclerview, parent, false)
-        val holder = SongItemHolder(view)
-
-        return holder
+        return SongItemHolder(view)
     }
 
     override fun getItemCount(): Int {
         return songs.size
-    }
-
-    fun setService(songs: List<Song>) {
-//        (context as MainActivity).bindPlayService(songs)
-        Log.d("SongAdapter", "songs ${songs}")
-//        context.setPlaylist(songs)
     }
 
     inner class SongItemHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
@@ -56,9 +42,8 @@ class SongAdapter(val songs: List<Song>, val context: Context): RecyclerView.Ada
             // split filename extension
             val tokens = song.name.split(".")
 
-            textViewName.setText(tokens[0])
-            textViewArtist.setText(song.artist)
+            textViewName.text = tokens[0]
+            textViewArtist.text = song.artist
         }
     }
-
 }
